e36f0f769f150dc463a7ed385a36161a0993b75d
3m1n3nc3 <mygames.ng@gmail.com>
2022-09-04 16:49:26 +0100

    Fix secure file route

--- routes/routes.php
+++ routes/routes.php
@@ -4,4 +4,4 @@ use ToneflixCode\LaravelFileable\Media;
 
 Route::get(config('toneflix-fileable.secure_file_route', 'load/images/{file}'), function ($file) {
     return (new Media)->privateFile($file);
-})->middleware(config('secure_image_middleware') ? ['window_auth'] : [])->name('fileable.secure.file');
+})->middleware(config('toneflix-fileable.secure_file_middleware', []) ?? [])->name('fileable.secure.file');
