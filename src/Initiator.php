c4611fedcf14225803596bad42f8c01b01164fd4
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2023-12-20 12:44:02 +0100

    Allow secure urls to be returned

--- src/Initiator.php
+++ src/Initiator.php
@@ -34,41 +34,18 @@ class Initiator
     }
 
     /**
-     * Encode data to Base64URL
+     * Load public asset
      *
-     * @param  string  $data
-     * @return bool|string
+     * @return string
      */
-    public static function base64url_encode($data)
+    public static function asset(string $url, $absolute = false): string
     {
-        // First of all you should encode $data to Base64 string
-        $b64 = base64_encode($data);
-
-        // Make sure you get a valid result, otherwise, return FALSE, as the base64_encode() function do
-        if ($b64 === false) {
-            return false;
+        if ($absolute) {
+            return str($url)->replace('http:', request()->isSecure() ? 'https:' :'http:')->toString();
         }
 
-        // Convert Base64 to Base64URL by replacing “+” with “-” and “/” with “_”
-        $url = strtr($b64, '+/', '-_');
-
-        // Remove padding character from the end of line and return the Base64URL result
-        return rtrim($url, '=');
-    }
-
-    /**
-     * Decode data from Base64URL
-     *
-     * @param  string  $data
-     * @param  bool  $strict
-     * @return bool|string
-     */
-    public static function base64url_decode($data, $strict = false)
-    {
-        // Convert Base64URL to Base64 by replacing “-” with “+” and “_” with “/”
-        $b64 = strtr($data, '-_', '+/');
-
-        // Decode Base64 string and return the original data
-        return base64_decode($b64, $strict);
+        return request()->isSecure()
+            ? secure_asset($url)
+            : asset($url);
     }
 }
