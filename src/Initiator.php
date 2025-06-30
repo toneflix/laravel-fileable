663b1584cd75da4a3ee9c3f0df5020cf1bd3f0d5
3m1n3nc3 <52163001+3m1n3nc3@users.noreply.github.com>
2025-06-30 00:42:46 +0000

    Fix styling

--- src/Initiator.php
+++ src/Initiator.php
@@ -18,18 +18,18 @@ class Initiator
     public static function collectionPaths(): array|Collection
     {
         $deepPaths = collect(config('toneflix-fileable.collections', []))->map(function ($col, $key) {
-            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.path');
+            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.path');
             $prefix = str($key)->contains('private') ? '/' : '/public/';
 
             if (! isset($col['path'])) {
-                return collect($col)->filter(fn($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
-                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.' . $path . '.path');
+                return collect($col)->filter(fn ($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
+                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.'.$path.'.path');
 
-                    return Storage::path($prefix . $getPath);
+                    return Storage::path($prefix.$getPath);
                 })->values();
             }
 
-            return Storage::path($prefix . $getPath);
+            return Storage::path($prefix.$getPath);
         });
 
         return $deepPaths->flatten()->unique();
@@ -38,10 +38,10 @@ class Initiator
     /**
      * Load public asset
      */
-    public static function asset(string $url, $absolute = false,  ?Filesystem $disk = null): string
+    public static function asset(string $url, $absolute = false, ?Filesystem $disk = null): string
     {
-        if ($disk && !$disk instanceof \Illuminate\Filesystem\LocalFilesystemAdapter) {
-            return $disk->url('public/' . $url);
+        if ($disk && ! $disk instanceof \Illuminate\Filesystem\LocalFilesystemAdapter) {
+            return $disk->url('public/'.$url);
         }
 
         if ($absolute) {
