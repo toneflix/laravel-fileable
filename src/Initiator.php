8f72836a1fa0254f7c5bb33cab878195eb6ef877
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-06-30 01:42:19 +0100

    feat: Check if file is a local asset, call the $disk->url() method to retrive asset url if it's not local.

--- src/Initiator.php
+++ src/Initiator.php
@@ -2,6 +2,7 @@
 
 namespace ToneflixCode\LaravelFileable;
 
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
@@ -17,18 +18,18 @@ class Initiator
     public static function collectionPaths(): array|Collection
     {
         $deepPaths = collect(config('toneflix-fileable.collections', []))->map(function ($col, $key) {
-            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.path');
+            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.path');
             $prefix = str($key)->contains('private') ? '/' : '/public/';
 
             if (! isset($col['path'])) {
-                return collect($col)->filter(fn ($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
-                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.'.$path.'.path');
+                return collect($col)->filter(fn($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
+                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.' . $path . '.path');
 
-                    return Storage::path($prefix.$getPath);
+                    return Storage::path($prefix . $getPath);
                 })->values();
             }
 
-            return Storage::path($prefix.$getPath);
+            return Storage::path($prefix . $getPath);
         });
 
         return $deepPaths->flatten()->unique();
@@ -37,8 +38,12 @@ class Initiator
     /**
      * Load public asset
      */
-    public static function asset(string $url, $absolute = false): string
+    public static function asset(string $url, $absolute = false,  ?Filesystem $disk = null): string
     {
+        if ($disk && !$disk instanceof \Illuminate\Filesystem\LocalFilesystemAdapter) {
+            return $disk->url('public/' . $url);
+        }
+
         if ($absolute) {
             return str($url)->replace('http:', request(null)->isSecure() ? 'https:' : 'http:')->toString();
         }
