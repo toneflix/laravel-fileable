972c4af9eaae1ab741e2fd6e7c921195cf5c6eef
3m1n3nc3 <52163001+3m1n3nc3@users.noreply.github.com>
2025-06-30 06:55:32 +0000

    Fix styling

--- src/Initiator.php
+++ src/Initiator.php
@@ -19,18 +19,18 @@ class Initiator
     public static function collectionPaths(): array|Collection
     {
         $deepPaths = collect(config('toneflix-fileable.collections', []))->map(function ($col, $key) {
-            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.path');
+            $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.path');
             $prefix = str($key)->contains('private') ? '/' : '/public/';
 
             if (! isset($col['path'])) {
-                return collect($col)->filter(fn($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
-                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key . '.' . $path . '.path');
+                return collect($col)->filter(fn ($k) => isset($k['path']))->map(function ($k, $path) use ($key, $prefix) {
+                    $getPath = Arr::get(config('toneflix-fileable.collections', []), $key.'.'.$path.'.path');
 
-                    return Storage::path($prefix . $getPath);
+                    return Storage::path($prefix.$getPath);
                 })->values();
             }
 
-            return Storage::path($prefix . $getPath);
+            return Storage::path($prefix.$getPath);
         });
 
         return $deepPaths->flatten()->unique();
