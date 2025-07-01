697365b4eed2754421df1b7e32ceffa0277bd5aa
Legacy <52163001+3m1n3nc3@users.noreply.github.com>
2025-07-01 18:08:25 +0100

    fix: Use correct variable

--- src/Initiator.php
+++ src/Initiator.php
@@ -41,7 +41,7 @@ class Initiator
      */
     public static function asset(string $url, $absolute = false, ?Filesystem $disk = null): string
     {
-        if (filter_var($src, FILTER_VALIDATE_URL)) {
+        if (filter_var($url, FILTER_VALIDATE_URL)) {
             return $url;
         }
 
